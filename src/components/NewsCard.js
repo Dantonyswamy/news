b452ca8befc1b88a072cdf180e60fc38099e7c58
Dantony <dantonyswamy@realtermenergy.com>
2021-04-18 17:09:17 -0400

    finished styling and href for NewsCard

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -9,16 +9,33 @@ class NewsCard extends React.Component{
     componentDidMount() {       
         // console.log(this.imageRef.current.clientHeight)
         const height = this.imageRef.current.clientHeight;
-        const spans = Math.ceil(height / 10);
+        const spans = Math.ceil(height / 450);
         this.setState({spans})
     }
   
     render() {
-        const { title, urlToImage } = this.props.article;
-        return <div style={{gridRowEnd:`span ${this.state.spans}`}}>
-            <img ref={this.imageRef} alt={title} src={urlToImage}>                
-            </img>
-         </div>
+        const { title, urlToImage, description,url } = this.props.article;
+        return (            
+            <a
+                className="ui raised cards"
+                style={{ gridRowEnd: `span ${this.state.spans}` }}
+                href={url}
+                target="_blank"
+                rel='noreferrer'>
+                <div className="card">
+                    <div className="image">
+                        <img ref={this.imageRef} alt={title} src={urlToImage}>
+                        </img>
+                    </div>
+                    <div className="content">
+                        <div className="header">{title}</div>
+                        <div class="description">
+                { description }
+              </div>
+                 </div>   
+                </div>
+                </a>                
+        );
     }
 }
 
